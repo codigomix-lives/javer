3ba06b2cec4a479dc953d4c044252322230a36d3
primo santini <primosantini@brq.com>
2021-02-21 13:48:30 -0300

    recovering files

--- src/br/com/codigomix/javer/Main.java
+++ src/br/com/codigomix/javer/Main.java
@@ -1,15 +1,11 @@
 package br.com.codigomix.javer;
 
-<<<<<<< HEAD
 import java.nio.file.Path;
 
-=======
->>>>>>> e6553b88cdd330a8146293b5fc8456728a224d2b
 public class Main {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
 		if (args.length == 0) {
 			System.out.println("Favor informar a classe, biblioteca ou diretorio");
 			System.exit(1);
@@ -35,18 +31,5 @@ public class Main {
 
 		jc.printVersion();
 
-=======
-		switch (args[0]) {
-		case "-v":
-			JavaFile.of(args[1]).printVersion();
-			break;
-
-		case "-anotherUsefulUsageForThisProgram":
-			break;
-		
-		default:
-			System.out.println("Invalid option");
-		}
->>>>>>> e6553b88cdd330a8146293b5fc8456728a224d2b
 	}
 }
